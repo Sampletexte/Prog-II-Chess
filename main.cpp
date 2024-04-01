db438f9d10009ae18e88243748b6bac5eea9b4f2
Sampletexte <nicholasjosephhubbard@gmail.com>
2024-04-01 12:46:52 -0400

    **BUGGED**  logic to print out white pieces in gameploop

--- main.cpp
+++ main.cpp
@@ -2,12 +2,13 @@
 #include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
 #include <unordered_map>
-#include "/Board/Board.h"
+#include "Board/Board.h"
 
 
 
 
 int main() {
+
     sf::Texture board,wp,wr,wb,wn,wq,wk,bp,br,bb,bn,bq,bk;
     board.loadFromFile("Sprites/board.png");
     wp.loadFromFile("Sprites/Chess_plt45.svg.png");
@@ -23,48 +24,84 @@ int main() {
     bq.loadFromFile("Sprites/Chess_qdt45.svg.png");
     bk.loadFromFile("Sprites/Chess_kdt45.svg.png");
 
+
+    // Initialize class gameboard for use; set scale and proper background for gameboard
+    Board gameboard;
     sf::Sprite chessBoard;
     chessBoard.setTexture(board);
     chessBoard.setScale(.68493150684,.68610634648 );
+
+    //Create list of textures for white pieces
+    std::string whiteTextureList[6] = {"Sprites/Chess_plt45.svg.png","Sprites/Chess_rlt45.svg.png","Sprites/Chess_blt45.svg.png",
+                       "Sprites/Chess_nlt45.svg.png", "Sprites/Chess_qlt45.svg.png", "Sprites/Chess_klt45.svg.png"};
+
+    //Create list of texture for black pieces
+    std::string blackTextureList[6] = {"Sprites/Chess_pdt45.svg.png", "Sprites/Chess_rdt45.svg.png", "Sprites/Chess_bdt45.svg.png",
+                                       "Sprites/Chess_ndt45.svg.png", "Sprites/Chess_qdt45.svg.png", "Sprites/Chess_kdt45.svg.png"};
+
+    //Create list of
+    char charList[6] = {'p','b','q','k','r','n'};
+
+
+    std::unordered_map<char, sf::Sprite >  whiteTextureMap{};
+    std::unordered_map<char, sf::Sprite > blackTextureMap{};
+
+
+
+    for(int i = 0; i < 6; i++){
+        sf::Texture tempTexture;
+        tempTexture.loadFromFile(whiteTextureList[i]);
+        whiteTextureMap[charList[i]] = sf::Sprite(tempTexture);
+    }
+
+    for(int i = 0; i < 6; i++) {
+        sf::Texture tempTexture;
+        tempTexture.loadFromFile(blackTextureList[i]);
+        blackTextureMap[charList[i]] = sf::Sprite(tempTexture);
+    }
+
     sf::Sprite whitePawn;
     whitePawn.setTexture(wp);
     sf::Sprite whiteBishop;
     whiteBishop.setTexture(wb);
 
 
-    Board gameboard;
+
+
 
 
     sf::RenderWindow window(sf::VideoMode(800, 800), "Chess 2: The Sequel");
     sf::Event event;
-    while (window.isOpen()){
-        while(window.pollEvent(event)){
-            if(event.type == sf::Event::Closed){
-                window.close();
-            }
+        `while (window.isOpen()){
+            while(window.pollEvent(event)){
+                if(event.type == sf::Event::Closed){
+                    window.close();
+                }
 
-            window.clear();
-            window.draw(chessBoard);
-            for(int y_ind = 0; y_ind < 8; y_ind++ ){
-                for(int x_ind = 0; x_ind < 8; x_ind++ ){
-                    ChessPiece *tempPiece = gameboard.getPieceatPos(x_ind,y_ind);
-                    if(tempPiece->getSide() == WHITE){
-                        switch(tempPiece->getName()){
+                window.clear();
+                window.draw(chessBoard);
 
-                        }
+                for(int y_ind = 0; y_ind < 8; y_ind++ ){
+                    for(int x_ind = 0; x_ind < 8; x_ind++ ){
+                        ChessPiece *tempPiece = gameboard.getPieceatPos(x_ind,y_ind);
+                        if(tempPiece->getSide() == WHITE){
+                            sf::Sprite piece = whiteTextureMap[tempPiece->getName()];
+                            piece.setPosition(x_ind * 100, y_ind * 100);
+                            piece.setScale(.125,.125);
+                            window.draw(piece);
+                            }`
                     }
                 }
             }
-            whitePawn.setPosition(100,700);
-            whitePawn.setScale(.125,.125);
+
             window.display();
 
 
 
 
         }
+    return 0;
     }
 
 
-    return 0;
-}
+
