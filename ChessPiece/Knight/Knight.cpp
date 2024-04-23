b9a7aaf09f3dffbecfed80e761bb278917440846
jamartin03 <128872712+jamartin03@users.noreply.github.com>
2024-04-23 15:25:36 -0400

    updated logic

--- ChessPiece/Knight/Knight.cpp
+++ ChessPiece/Knight/Knight.cpp
@@ -1,11 +1,35 @@
 //
 // Created by sochin1519 on 3/25/2024.
 //
-
 #include "Knight.h"
 
-Knight::Knight() {
-    name = 'n';
-};
+Knight::Knight() {}
+
+std::vector<Point> Knight::getPossibleMoves(std::vector<std::vector<ChessPiece *>> * gameboard, int posX, int posY) {
+    std::vector<Point> posMovs;
+
+    // Define all possible knight moves relative to the current position
+    int possibleMoves[8][2] = {
+            {-1, -2}, {-2, -1}, {-2, 1}, {-1, 2},
+            {1, -2}, {2, -1}, {2, 1}, {1, 2}
+    };
+
+    // Check each possible move
+    for (auto move : possibleMoves) {
+        int newX = posX + move[0];
+        int newY = posY + move[1];
+
+        // Verify if the move is within the board boundaries
+        if (newX >= 0 && newX < 8 && newY >= 0 && newY < 8) {
+            ChessPiece *piece = (*gameboard)[newX][newY];
+            // Add the move to validMoves if the destination is empty or contains an opponent's piece
+            if (piece->getName() == 0 || piece->getSide() != side) {
+                posMovs.push_back(Point{newX, newY});
+            }
+        }
+    }
+
+    return posMovs;
+}
 
 //RelativeActions Knight::getPossibleMoves(Board *board, int x, int y) {};
--- ChessPiece/Queen/Queen.cpp
+++ ChessPiece/Queen/Queen.cpp
@@ -4,8 +4,97 @@
 
 #include "Queen.h"
 
-Queen::Queen() {
-    name = 'q';
-};
+Queen::Queen() {}
+
+std::vector<Point> Queen::getPossibleMoves(std::vector<std::vector<ChessPiece *>> *gameboard, int posX, int posY) {
+    std::vector<Point> posMovs;
+
+    // Check horizontally and vertically (up, down, left, right)
+    for (int i = 1; posX + i < 8; i++) {
+        if ((*gameboard)[posX + i][posY]->getName() == 0) {
+            posMovs.push_back(Point{posX + i, posY});
+        } else if ((*gameboard)[posX + i][posY]->getSide() != side) { // Opponent piece
+            posMovs.push_back(Point{posX + i, posY});
+            break;
+        } else { // Own piece blocking the way
+            break;
+        }
+    }
+    for (int i = 1; posX - i >= 0; i++) {
+        if ((*gameboard)[posX - i][posY]->getName() == 0) {
+            posMovs.push_back(Point{posX - i, posY});
+        } else if ((*gameboard)[posX - i][posY]->getSide() != side) { // Opponent piece
+            posMovs.push_back(Point{posX - i, posY});
+            break;
+        } else { // Own piece blocking the way
+            break;
+        }
+    }
+    for (int i = 1; posY + i < 8; i++) {
+        if ((*gameboard)[posX][posY + i]->getName() == 0) {
+            posMovs.push_back(Point{posX, posY + i});
+        } else if ((*gameboard)[posX][posY + i]->getSide() != side) { // Opponent piece
+            posMovs.push_back(Point{posX, posY + i});
+            break;
+        } else { // Own piece blocking the way
+            break;
+        }
+    }
+    for (int i = 1; posY - i >= 0; i++) {
+        if ((*gameboard)[posX][posY - i]->getName() == 0) {
+            posMovs.push_back(Point{posX, posY - i});
+        } else if ((*gameboard)[posX][posY - i]->getSide() != side) { // Opponent piece
+            posMovs.push_back(Point{posX, posY - i});
+            break;
+        } else { // Own piece blocking the way
+            break;
+        }
+    }
+
+    // Check diagonally (up-right, up-left, down-right, down-left)
+    for (int i = 1; posX + i < 8 && posY - i >= 0; i++) {
+        if ((*gameboard)[posX + i][posY - i]->getName() == 0) {
+            posMovs.push_back(Point{posX + i, posY - i});
+        } else if ((*gameboard)[posX + i][posY - i]->getSide() != side) { // Opponent piece
+            posMovs.push_back(Point{posX + i, posY - i});
+            break;
+        } else { // Own piece blocking the way
+            break;
+        }
+    }
+    for (int i = 1; posX - i >= 0 && posY - i >= 0; i++) {
+        if ((*gameboard)[posX - i][posY - i]->getName() == 0) {
+            posMovs.push_back(Point{posX - i, posY - i});
+        } else if ((*gameboard)[posX - i][posY - i]->getSide() != side) { // Opponent piece
+            posMovs.push_back(Point{posX - i, posY - i});
+            break;
+        } else { // Own piece blocking the way
+            break;
+        }
+    }
+    for (int i = 1; posX + i < 8 && posY + i < 8; i++) {
+        if ((*gameboard)[posX + i][posY + i]->getName() == 0) {
+            posMovs.push_back(Point{posX + i, posY + i});
+        } else if ((*gameboard)[posX + i][posY + i]->getSide() != side) { // Opponent piece
+            posMovs.push_back(Point{posX + i, posY + i});
+            break;
+        } else { // Own piece blocking the way
+            break;
+        }
+    }
+    for (int i = 1; posX - i >= 0 && posY + i < 8; i++) {
+        if ((*gameboard)[posX - i][posY + i]->getName() == 0) {
+            posMovs.push_back(Point{posX - i, posY + i});
+        } else if ((*gameboard)[posX - i][posY + i]->getSide() != side) { // Opponent piece
+            posMovs.push_back(Point{posX - i, posY + i});
+            break;
+        } else { // Own piece blocking the way
+            break;
+        }
+    }
+
+    return posMovs;
+}
+
 
 //RelativeActions Queen::getPossibleMoves(Board *board, int x, int y) {};
